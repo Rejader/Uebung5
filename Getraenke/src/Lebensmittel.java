1d288b160893087729ddb822a5ff54c590f405ea
Maximilian <Maximilian@DESKTOP-BBQJ7D2>
2019-02-03 16:41:57 +0100

    classes

--- Getraenke/src/Lebensmittel.java
+++ Getraenke/src/Lebensmittel.java
@@ -1,4 +1,19 @@
+public abstract class Lebensmittel {
 
-public class Lebensmittel {
+    protected String name;
+    protected int menge;
 
+    public Lebensmittel(String name, int menge) {
+        this.name = name;
+        this.menge = menge;
+    }
+
+    public abstract boolean essen();
+    public abstract boolean trinken();
+
+    public String status() {
+        return "Klasse: " + this.getClass().getName() +
+                " Instance: " + this.name +
+                " Menge: " + this.menge;
+    }
 }
